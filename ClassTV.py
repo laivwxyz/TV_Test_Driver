98334ecc5646dad888b7834f305518825188bfda
laivwxyz <delacruzlailanie2004@gmail.com>
2023-05-17 22:53:34 +0800

    Creating the class instances

--- ClassTV.py
+++ ClassTV.py
@@ -1,3 +1,7 @@
 # Creating the class named TV
 class TV:
-    def __init__(self, channel, volume_level):
+    def __init__(self, channel=1, volume_level=1):
+        # creating the class instances
+        self.channel = channel
+        self.volume_level = volume_level
+        self.tv_on = True 
