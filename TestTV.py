b7260fae7c5f5aea6800a932c531ef083a7dc192
laivwxyz <delacruzlailanie2004@gmail.com>
2023-05-18 10:49:59 +0800

    Creating the first TV odject

--- TestTV.py
+++ TestTV.py
@@ -1,2 +1,16 @@
 # Importing class TV from ClassTV.py
 from ClassTV import TV
+
+def test_tv():
+    # Creating the first TV object
+    tv1 = TV()
+    tv1.turn_on()
+    tv1.get_channel()
+    tv1.set_channel()
+    tv1.get_volume()
+    tv1.set_volume()
+    tv1.channel_up()
+    tv1.channel_down()
+    tv1.volume_up()
+    tv1.volume_down()
+    
