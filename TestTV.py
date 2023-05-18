8f337c41df799302db79a44585be37d7c88a3242
laivwxyz <delacruzlailanie2004@gmail.com>
2023-05-18 10:53:29 +0800

    Sets channel and volume for the first TV

--- TestTV.py
+++ TestTV.py
@@ -6,11 +6,10 @@ def test_tv():
     tv1 = TV()
     tv1.turn_on()
     tv1.get_channel()
-    tv1.set_channel()
+    tv1.set_channel(30)
     tv1.get_volume()
-    tv1.set_volume()
+    tv1.set_volume(3)
     tv1.channel_up()
     tv1.channel_down()
     tv1.volume_up()
     tv1.volume_down()
-    
